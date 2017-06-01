23344d2da5cd9c2e225be21f317db0e4638e1cb6
Meta Emilie Ünal Hansen <lekatmeta@gmail.com>
2017-06-01 18:01:40 +0200

    Created all the domain models

---
+++ DomainModel/Orders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel
+{
+    class Orders
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
